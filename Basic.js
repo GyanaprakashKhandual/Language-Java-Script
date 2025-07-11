a0207252bfee99236a96cad02eea27f591a212c8
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-07-11 19:57:09 +0530

    Rain started

---
+++ Basic.js
@@ -0,0 +1,12 @@
+const printName = (name) => {
+    return name;
+}
+
+console.log(printName('Chris'));
+
+
+const greet = (name) => {
+    return name + 'You are the best in the world';
+}
+
+console.log(greet('chris'));
